ce478ea020114e75bb01b1aec97959805957206f
Ajinkya Shinde <ajinkyashinde15@gmail.com>
2017-05-24 01:12:37 +0100

    GUI Completion
    Starting coding in HomeActivity.java claass

--- app/src/main/java/com/brewery/beer/beerbreweryandroid/HomeActivity.java
+++ app/src/main/java/com/brewery/beer/beerbreweryandroid/HomeActivity.java
@@ -2,12 +2,28 @@ package com.brewery.beer.beerbreweryandroid;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.MultiAutoCompleteTextView;
+import android.widget.TextView;
 
 public class HomeActivity extends AppCompatActivity {
 
     @Override
+    Button nextButton;
+    TextView beerTitle;
+    ImageView beerImageView;
+    MultiAutoCompleteTextView beerDescription;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        nextButton=(Button)findViewById(R.id.nextButton);
+        beerTitle=(TextView)findViewById(R.id.beerTitle);
+        beerImageView=(ImageView)findViewById(R.id.beerImageView);
+        beerDescription=(MultiAutoCompleteTextView)findViewById(R.id.beerDescription);
+
+
     }
 }
